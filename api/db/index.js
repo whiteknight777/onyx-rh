85cbcd8e703aafe6e372bb834dcc2498cf6659db
Desire ARRA <desirearra@gmail.com>
2022-01-25 15:29:07 +0100

    update api content & deploy API with heroku

--- api/db/index.js
+++ api/db/index.js
@@ -1,20 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// ### ATLAS CONNECTION
+let DB_URI =
+  "mongodb+srv://DB_USER:DB_USER_PASS@DB_CLUSTER.bcnan.mongodb.net/DB_NAME?retryWrites=true&w=majority";
+let DB_USER = "rocket-admin";
+let DB_USER_PASS = "od1dzvGIgAEX56rU";
+let DB_CLUSTER = "rocketcluster";
+let DB_NAME = "rocket-crm";
 
 // eslint-disable-next-line
-let DB_URI = process.env.DB_URI
-const {
-    DB_USER, DB_USER_PASS,
-    DB_CLUSTER,
-    DB_NAME
-} = process.env;
+// let DB_URI = process.env.DB_URI
+// const {
+//     DB_USER, DB_USER_PASS,
+//     DB_CLUSTER,
+//     DB_NAME
+// } = process.env;
 
 // eslint-disable-next-line
-String.prototype.replaceArray = function (find, replace) {
-    let replaceString = this;
-    for (let i = 0; i < find.length; i++) {
-        replaceString = replaceString.replace(find[i], replace[i]);
-    }
-    return replaceString;
+String.prototype.replaceArray = function(find, replace) {
+  let replaceString = this;
+  for (let i = 0; i < find.length; i++) {
+    replaceString = replaceString.replace(find[i], replace[i]);
+  }
+  return replaceString;
 };
 
 const find = [/DB_USER/i, /DB_USER_PASS/i, /DB_CLUSTER/i, /DB_NAME/i];
--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,17 @@
-const proxy = require('http-proxy-middleware');
-const morgan = require('morgan');
+const proxy = require("http-proxy-middleware");
+const morgan = require("morgan");
 
-let { ALLOWED_ORIGINS } = process.env;
-ALLOWED_ORIGINS = ALLOWED_ORIGINS.split(',');
+const ALLOWED_ORIGINS = "https://master.d3c7umzhd3tk9k.amplifyapp.com";
+// const ALLOWED_ORIGINS = "http://localhost:3005";
 
-module.exports = app => {
-    app.use(
-        '/api',
-        proxy({
-            target: ALLOWED_ORIGINS[0],
-            changeOrigin: true
-        }),
-    );
+module.exports = (app) => {
+  app.use(
+    "/api",
+    proxy({
+      target: ALLOWED_ORIGINS,
+      changeOrigin: true,
+    })
+  );
 
-    app.use(morgan('combined'));
+  app.use(morgan("combined"));
 };
